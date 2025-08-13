8781b146168b91daa1ca8d702783c61ff4314859
Mathias Møller Lauridsen <mail@mtlr.dk>
2025-08-13 19:39:46 +0200

    refactor: move files into root folder

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,13 +3,10 @@
 echo "Installerer nødvendige pakker..."
 
 sudo apt update
-sudo apt install -y stow git curl neofetch zsh fzf
-
-# Installer OhMyZsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo apt install -y git curl stow neofetch zsh fzf
 
 # Installer Starship
-curl -fsSL https://starship.rs/install.sh | sh
+# curl -fsSL https://starship.rs/install.sh | sh
 
 # Indstil zsh som standard shell
 echo "Indstiller zsh som standard shell"
@@ -18,27 +15,7 @@ chsh -s $(which zsh)
 # Gå til roden af dotfiles-repoet (en mappe op fra hvor scriptet ligger)
 cd "$(dirname "$0")/.."
 
-# Funktion til at tage backup af en eksisterende fil
-backup_file() {
-    local file="$1"
-    if [ -e "$file" ] && [ ! -L "$file" ]; then
-        local backup_file="${file}.$(date +%Y%m%d%H%M%S).backup"
-        echo "Backing up $file to $backup_file"
-        mv "$file" "$backup_file"
-    fi
-}
-
-echo "Backing up existing files"
-
 echo "Stowing all packages..."
-for pkg in $(find . -maxdepth 1 -type d -not -name ".git" -not -name ".*" -not -name "scripts" -exec basename {} \;); do
-    echo "Stowing $pkg..."
-    # Find alle filer i pakken, og tag backup hvis nødvendigt
-    for dotfile in "$(find "$pkg" -type f)"; do
-        target="$HOME/$(basename "$dotfile")"
-        backup_file "$target"
-    done
-    stow -R "$pkg"
-done
+stow .
 
 echo "All packages stowed!"
