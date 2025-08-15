cc267546aa10efe38e15693473ba5548d099158e
Mathias Møller Lauridsen <mail@mtlr.dk>
2025-08-15 15:42:38 +0200

    fix: customize neovim, disable arrowkeys

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -10,7 +10,23 @@ map("n", "<C-l>", "<cmd>TmuxNavigateRight<CR>", { desc = "Navigate to right pane
 map("n", "<C-j>", "<cmd>TmuxNavigateDown<CR>", { desc = "Navigate to bottom pane" })
 map("n", "<C-k>", "<cmd>TmuxNavigateUp<CR>", { desc = "Navigate to upper pane" })
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
+-- map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+
+-- Deactivate the arrow keys in normal-, insert- and visualmode
+vim.api.nvim_set_keymap('n', '<Up>', '<NOP>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Down>', '<NOP>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Left>', '<NOP>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Right>', '<NOP>', { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('i', '<Up>', '<NOP>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<Down>', '<NOP>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<Left>', '<NOP>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', '<Right>', '<NOP>', { noremap = true, silent = true })
+
+vim.api.nvim_set_keymap('v', '<Up>', '<NOP>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<Down>', '<NOP>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<Left>', '<NOP>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<Right>', '<NOP>', { noremap = true, silent = true })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
